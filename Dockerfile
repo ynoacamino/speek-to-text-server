2efc06213e0ba8a1384256e80937f1a736f9d91b
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2025-01-16 16:49:12 -0500

    add spanish model

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,15 @@
 FROM python:3.9-slim
 
 RUN apt-get update && apt-get install -y \
+    git \
     build-essential \
+    libasound2-dev \
+    libportaudio2 \
     libatlas-base-dev \
+    ffmpeg \
     && rm -rf /var/lib/apt/lists/*
+
+ENV POCKETSPHINX_PATH=/app/model
 
 WORKDIR /app
 
@@ -11,8 +17,9 @@ COPY requirements.txt ./
 
 RUN pip install --no-cache-dir -r requirements.txt
 
-COPY main.py ./
+COPY . .
 
 EXPOSE 5000
 
-CMD ["flask", "--app", "main", "run", "--host", "0.0.0.0"]
+# flag to verbose logs
+CMD ["gunicorn", "-w", "1", "-b", "0.0.0.0:5000", "main:app"]
--- main.py
+++ main.py
@@ -1,20 +1,55 @@
-from flask import Flask
-from flask import request
-import speech_recognition as sr
+from flask import Flask, request, jsonify
+from pocketsphinx import AudioFile, get_model_path
+from pydub import AudioSegment
+
+
 app = Flask(__name__)
 app.config["MAX_CONTENT_LENGTH"] = 50 * 1000 * 1000
-r = sr.Recognizer()
+
+
+@app.get("/env")
+def env():
+    model_path = get_model_path()
+    return jsonify({"model_path": model_path})
 
 @app.post("/recognize")
 def recognize():
-    audio_data = request.files["audio"]
-    with sr.AudioFile(audio_data) as source:
-        audio = r.record(source)
-    lang = request.form["lang"]
+    audio_file = request.files["audio"]
+    original_path = f"/tmp/{audio_file.filename}"
+    converted_path = f"/tmp/converted_{audio_file.filename}"
+    audio_file.save(original_path)
+
+    # Convertir a WAV compatible con Pocketsphinx
     try:
-        result = r.recognize_sphinx(audio, language=lang)
-    except sr.UnknownValueError:
-        result = "Could not understand audio"
-    except sr.RequestError as e:
-        result = f"Error: {e}"
-    return {"result": result}
+        audio = AudioSegment.from_file(original_path)
+        audio = audio.set_frame_rate(16000).set_sample_width(2).set_channels(1)
+        audio.export(converted_path, format="wav")
+    except Exception as e:
+        return jsonify({"error": f"Error al convertir el audio: {str(e)}"}), 400
+
+
+    model_path = get_model_path()
+
+    config = {
+        "verbose": False,
+        "audio_file": converted_path,
+        "hmm": model_path + "/es",
+        "lm": model_path + "/es.lm.gz",
+        "dict": model_path + "/es-dic.dic",
+    }
+
+    try:
+        audio = AudioFile(**config)
+        print("test", audio)
+        transcript = ""
+        for phrase in audio:
+            print("phrase", phrase)
+            transcript += str(phrase) + " "
+    except Exception as e:
+        return jsonify({"error": f"Error al procesar el audio: {str(e)}"}), 500
+
+    # Devuelve el texto transcrito
+    return jsonify({"transcription": transcript})
+
+if __name__ == "__main__":
+    app.run()
